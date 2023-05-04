719ba3471a1bd9b8e769fcdb5f0545b229d072b2
Eddy CHAN Yuen Ho <PT4481@cmhk.local>
2023-05-04 11:25:20 +0800

    added form

--- english-learning/src/page/home.js
+++ english-learning/src/page/home.js
@@ -1,11 +1,104 @@
 import React from "react";
+import { Button, Form, Input, Radio } from "antd";
 
-export default function home() {
+const onFinish = (values) => {
+  console.log("Success:", values);
+};
+
+const onFinishFailed = (errorInfo) => {
+  console.log("Failed:", errorInfo);
+};
+
+export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-      Vocab store :<input />
-      <button>Hi</button>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+        flexDirection: "column",
+      }}
+    >
+      <h1>English form</h1>
+      <Form
+        name="basic"
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+        style={{ fontSize: "150%" }}
+      >
+        <Form.Item
+          label="Word"
+          name="word"
+          rules={[
+            {
+              required: true,
+              message: "Please input the word!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          name="type"
+          valuePropName="checked"
+          label="Type"
+          rules={[
+            {
+              required: true,
+              message: "Please select the word type!",
+            },
+          ]}
+        >
+          <Radio.Group>
+            <Radio.Button value="easy">Easy</Radio.Button>
+            <Radio.Button value="mid">Mid</Radio.Button>
+            <Radio.Button value="hard">Hard</Radio.Button>
+          </Radio.Group>
+        </Form.Item>
+
+        <Form.Item
+          label="Sentence"
+          name="sentence"
+          rules={[
+            {
+              required: true,
+              message: "Please input the sentence",
+            },
+          ]}
+        >
+          <Input.TextArea />
+        </Form.Item>
+
+        <Form.Item
+          label="My sentence"
+          name="mySentence"
+          rules={[
+            {
+              required: true,
+              message: "Please create  sentence",
+            },
+          ]}
+        >
+          <Input.TextArea />
+        </Form.Item>
+
+        <Form.Item
+          wrapperCol={{
+            offset: 10,
+            span: 16,
+          }}
+        >
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   );
 }
